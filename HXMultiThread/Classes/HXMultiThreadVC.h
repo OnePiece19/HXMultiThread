7b47e3cb578aa408cd59575de4a770319a62bfa9
OnePiece19 <381509610@qq.com>
2021-08-20 03:37:01 +0800

    op

---
+++ HXMultiThread/Classes/HXMultiThreadVC.h
@@ -0,0 +1,16 @@
+//
+//  HXMultiThreadVC.h
+//  HXMultiThread
+//
+//  Created by HX on 2021/8/20.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface HXMultiThreadVC : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ HXMultiThread/Classes/HXMultiThreadVC.m
@@ -0,0 +1,31 @@
+//
+//  HXMultiThreadVC.m
+//  HXMultiThread
+//
+//  Created by HX on 2021/8/20.
+//
+
+#import "HXMultiThreadVC.h"
+
+@interface HXMultiThreadVC ()
+
+@end
+
+@implementation HXMultiThreadVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
