e2aedf8dfae555574ff6896d79ebc50ccfc22019
OnePiece19 <381509610@qq.com>
2021-08-20 03:21:49 +0800

    duoxiancheng

---
+++ HXMultiThread/Classes/多线程/TETS.h
@@ -0,0 +1,16 @@
+//
+//  TETS.h
+//  FBSnapshotTestCase
+//
+//  Created by HX on 2021/8/20.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TETS : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ HXMultiThread/Classes/多线程/TETS.m
@@ -0,0 +1,31 @@
+//
+//  TETS.m
+//  FBSnapshotTestCase
+//
+//  Created by HX on 2021/8/20.
+//
+
+#import "TETS.h"
+
+@interface TETS ()
+
+@end
+
+@implementation TETS
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
